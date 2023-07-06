8c5cda635f105c1494a789afab9a4ef9bab55c60
Jared Gergen <gergen.jared1@gmail.com>
2023-07-05 20:33:02 -0700

    Trying to resolve rendering issue

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+// src/components/About.js
+
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Add your about page content here */}
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Add your home page content here */}
+    </div>
+  );
+};
+
+export default Home;
